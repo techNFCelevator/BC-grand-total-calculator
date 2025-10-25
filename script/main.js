e36da2f530472836c63294721559d208f77b927f
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-10-24 21:44:23 -0700

    Fetch function for taxes data added

--- script/main.js
+++ script/main.js
@@ -1,2 +1,17 @@
+const price = document.getElementById('priceInput');
 const taxesFilePath = 'data/taxes.json';
 let taxes=[];
+fetch(taxesFilePath)
+  .then(
+    function(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Unable to obtain ${taxesFilePath} because the response is ${response.ok}');
+        }
+    }
+  )
+  .then(data => {
+    taxes = data;
+  })
+  .catch(error => console.error('Error loading taxes:', error));
