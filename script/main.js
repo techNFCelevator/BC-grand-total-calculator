1c621394e91b626536b47114aab586c0ef4bf2af
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-10-26 00:22:38 -0700

    Added no tax loaded error message

--- script/main.js
+++ script/main.js
@@ -1,7 +1,11 @@
+const display = document.querySelector("main");
+const popup = document.createElement("popup");
 const price = document.getElementById('priceInput');
 const taxesFilePath = 'data/taxes.json';
 let taxes = [];
 let errorMsg = "";
+popup.className = "popup";
+display.prepend(popup);
 fetch(taxesFilePath)
   .then(
     response => {
@@ -12,15 +16,13 @@ fetch(taxesFilePath)
         }
     }
   )
-  .then(data => {
+  .then(function(data) {
     taxes = data;
-    for (let key = 0; key < taxes.length;) {
-        if (Object.hasOwn(taxes[key], "tax") && typeof taxes[key].tax === "string" && Object.hasOwn(taxes[key], "rate") && typeof taxes[key].rate === "number") {
-            ++key;
-        } else {
-            taxes.splice(key, 1);
-        }
-    }
   })
-  .catch(error => console.error('Error loading taxes:', error));
-  if (taxes.length > 0) {}
+  .catch(error => errorMsg += error.message);
+  if (taxes.length > 0) {
+    price.removeAttribute("disabled");
+  } else {
+    errorMsg += "No tax loaded successfully.";
+    $(`.popup`).html(`<h2>ERROR</h2><p>${errorMsg}</p>`);
+  }
