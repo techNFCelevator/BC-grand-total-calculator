6cb5a7b8810cc4b5178fc0d1745d19f9dcc2b618
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-10-30 22:18:48 -0700

    Created function to calculate total

--- script/main.js
+++ script/main.js
@@ -29,3 +29,10 @@ fetch(taxesFile)
       }
     })
     .catch(error => $(`.popup`).html(`<h2>Error Loading Taxes</h2><p>${error.message}</p>`));
+    function calculate(subtotal) {
+      let total = subtotal;
+      taxes.forEach(tax => {
+        total += subtotal * (tax.rate / 100);
+      });
+      return total;
+    }
